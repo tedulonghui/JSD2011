c445b25f3754ea5806923378079933c3098c983f
longenheng <lh1057899208@163.com>
2021-01-19 18:30:19 +0800

    第一次使用github

---
+++ src/main/java/string/ListDemo.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.*;
+
+public class ListDemo {
+    public static void main(String[] args) {
+//        Map<String,String> map = new HashMap();
+//        Collection list = new ArrayList();
+//        list.add(1);
+//        list.add(2);
+//        list.add(4);
+//        list.add(5);
+//        System.out.println(list);
+//        System.out.println(list);
+    }
+
+}
